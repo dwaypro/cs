5517adaecb232354e7c30155702470534ca66851
unknown <dangrantway@gmail.com>
2019-02-15 06:29:07 -0700

    bubble sort

--- algorithms/sortingbubble.js
+++ algorithms/sortingbubble.js
@@ -7,8 +7,33 @@ function swap(arr, idx1, idx2){
     var temp = arr[idx1];
     arr[idx1] = arr[idx2];
     arr[idx2] = temp;
-}
+};
 
 const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
+
+
+
+function bubbleSort(arr){
+    var noSwaps; //optimization
+    for(var i = arr.length; i > 0; i--){
+        noSwaps = true;
+        for(var j = 0; j < i-1; j++){
+            console.log('arr', arr);
+            if(arr[j] > arr[j+1]){
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false;
+            }
+        }   
+        if(noSwaps) break;
+    }
+    return arr;
 }
+
+
+// bubbleSort([37,45,29,8])
+
+bubbleSort([1,2,3,8,4,5,6])
