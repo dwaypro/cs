3ec11f9752c3035f0774d764f084cd8ba2b73868
unknown <dangrantway@gmail.com>
2019-02-26 07:51:48 -0700

    adjacency matrix vs adjacency list

---
+++ datastructures/graphs.js
@@ -0,0 +1,23 @@
+//vertex - a node;
+//edge - connection between nodes;
+// weighted/unweighted
+    // unweighted
+    //  - edges aren't assigned a value.
+    // weighted
+    //  - values are assigned to edges. 
+//directed vs undirected
+    //undirected
+    // - no direction or polarity to a directon
+    // - an edge could be two way. 
+    // - Facebook as an example.
+    //directed
+    // - usually visualized with arrows. We can traverse one way
+    // - Instagram/Twitter vs Facebook. 
+// Adjacency Matrix
+    // - we have vertices and edges and we need to store it. 
+    // - two dimential structure usually nested arrays. 
+    // - between any two nodes     
+// Adjacency List
+    // - use an array or preferable a hashtable or list to store the edges
+    // - go to index 3 of our array for example, which
+    // will return the connection.     
