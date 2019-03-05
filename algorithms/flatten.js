da4dc06d9a52ccb38250155610fe3b7bba2bc1c3
unknown <dangrantway@gmail.com>
2019-03-05 07:40:14 -0700

    flatten

---
+++ algorithms/flatten.js
@@ -0,0 +1,20 @@
+function flatten(arr){ 
+    array2.join().split(',').map(function(item){
+     return item = parseInt(item);
+ });
+ 
+ }
+ 
+ 
+ function flatten2(arr){
+     var newArr = []
+       for(var i = 0; i < oldArr.length; i++){
+         if(Array.isArray(oldArr[i])){
+               newArr = newArr.concat(flatten(oldArr[i]))
+         } else {
+               newArr.push(oldArr[i])
+         }
+   } 
+   return newArr;
+ }
+ 
