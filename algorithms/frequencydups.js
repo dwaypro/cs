bffd5f17947c093a98b845a216d0aaf04d6667a4
unknown <dangrantway@gmail.com>
2019-03-07 07:04:14 -0700

    frequency dups

--- algorithms/frequencydups.js
+++ algorithms/frequencydups.js
@@ -3,6 +3,8 @@
  * a variable number of arguments, and checks whether there are any 
  * duplicates among the arguments passed in.
  */
+
+ //my implementation, using the frequency counter approach.
 function areThereDuplicates(){
     var args = Array.from(arguments);     
     var frequencyObject = {};
@@ -14,5 +16,33 @@ function areThereDuplicates(){
         } 
     }
     return false;
-
 }
+
+
+/**
+ * alternative solutions:
+ */
+// one liner kinda pretty:
+function areThereDuplicates() {
+    return new Set(arguments).size !== arguments.length;
+}
+
+//multiple pointers:
+function areThereDuplicates(...args) {
+    // ewww:
+    args.sort((a,b) => a > b);
+    // end eww
+
+    //'pointer' 1
+    let start = 0;
+    //'pointer' 2
+    let next = 1;
+    while(next < args.length){
+      if(args[start] === args[next]){
+          return true
+      }
+      start++
+      next++
+    }
+    return false
+  }
