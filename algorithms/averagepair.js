f44b2ca267130276d89cfa5de0a1e399cce242cb
unknown <dangrantway@gmail.com>
2019-03-08 07:29:34 -0700

    average pair

---
+++ algorithms/averagepair.js
@@ -0,0 +1,38 @@
+/*
+Write a function called averagePair. Given a sorted array
+of integers and a target average, determine if there is a pair
+of values in the array where the average pair of the pair equals
+the target average. There may be more than one pair that matches the average
+target.
+*/// 
+
+/**
+ * Helper function, that returns the average of two values passed in.
+ * @param {first integer to be averaged} val1 
+ * @param {*second integer to be averaged} val2 
+ */
+function average(val1, val2){
+    return (val1 + val2) / 2; 
+}
+
+/**
+ * Returns true if any pair in a given array maches the target average.
+ * @param arr the sorted array where the pairs are checked.
+ * @param target the target average.
+ */
+function averagePair(arr, target){
+    let start = 0;
+    let end = arr.length-1;
+    
+    while(start < end){
+       let averageVal = average(arr[start], arr[end]);
+       if(averageVal === target){
+           return true;
+       }else if (averageVal > target){
+         end--
+       }else if (averageVal < target){
+         start++   
+       }       
+    }
+    return false;   
+}
